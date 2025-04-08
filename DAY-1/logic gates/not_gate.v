51cdffccda61575e22c30f9f3423b0d1af6c0a36
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-04-08 12:03:28 +0530

    Update not_gate.v

--- DAY-1/logic gates/not_gate.v
+++ DAY-1/logic gates/not_gate.v
@@ -21,3 +21,25 @@ module not_gate(y,a);
    y=~a;
   
 endmodule
+
+// Testbench
+module not_gate_tb;
+  reg a;
+  wire y;
+ not_gate dut (
+    .y(y),
+    .a(a)
+  );
+
+  initial begin
+    $display("Time\t a\t y");
+    $monitor("%0t\t %b\t %b", $time, a, y);
+
+    a = 0; #10;
+    a = 1; #10;
+
+    
+    $finish;
+  end
+endmodule
+
