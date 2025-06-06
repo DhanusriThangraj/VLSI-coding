0408ec77782a060ea405fdfba5db8972a8a2bf1e
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-06-06 15:58:50 +0530

    Update one delay.v

--- DAY-8/Example programs/gate_delays/one delay.v
+++ DAY-8/Example programs/gate_delays/one delay.v
@@ -1,18 +1,21 @@
 // design code
-module gate_delays (input 	a, b,
-            	output out1, out2 ,out3);
-  and 		#(7) o1 (out1, a, b);
+module gate_delays (input a, b,
+                  output out1, out2 ,out3);
+
+	
+  and       #(7) o1 (out1, a, b);
   bufif0 	#(3) b1 (out2, a, b);
-  or      #(5) a1 (out3, a, b);
+  or        #(5) a1 (out3, a, b);
 
 endmodule
+
 
 // testbench
 module gate_delays_tb;
   reg a, b;
   wire out1, out2, out3;
-  gate_delays uut (.out1(out1), .out2(out2),.out3(out3) .a(a), .b(b));
-  initial begin
+ gate_delays uut (a,b,out1,out2,out3);
+initial begin
     $display("Time\ta\tb\tout1\tout2\tout3");
     $monitor("%0t\t%b\t%b\t%b\t%b\t%b",$time,a,b,out1,out2,out3);
     
