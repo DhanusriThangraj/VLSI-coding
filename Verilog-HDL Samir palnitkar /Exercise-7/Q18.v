98fee315ce121a5c4d158dd6a291fa56624341f3
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-06-11 10:28:10 +0530

    Create Q18.v

---
+++ Verilog-HDL Samir palnitkar /Exercise-7/Q18.v
@@ -0,0 +1,3 @@
+// 18. Design an 8-bit counter by using a forever loop, named block, and disabling of named block. 
+//   The counter starts counting at count = 5 and finishes at count = 67. The count is incremented at positive edge of clock.
+//   The clock has a time period of 10. The counter counts through the loop only once and then is disabled. (Hint: Use the disable statement).
