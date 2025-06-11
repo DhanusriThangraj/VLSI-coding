2a4aa6c07ead71f6d49f0c0551302489003e93cb
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-06-11 19:31:10 +0530

    Create Q1.v

---
+++ VERILOG TEST/TEST-1/Q1.v
@@ -0,0 +1,26 @@
+// 1. 1 22 333 4444 55555 4444 333 22 1 display this sequence in diamond shape 
+// design code
+module diamond_shape;
+    initial begin
+  $display("1      ");
+  $display("22      ");
+  $display("333      ");
+  $display("4444      ");
+  $display("55555      ");
+  $display("4444      ");
+  $display("333      ");
+  $display("22      ");
+  $display("1      ");
+    end
+endmodule
+
+// output
+1      
+22      
+333      
+4444      
+55555      
+4444      
+333      
+22      
+1 
