e494ec8a6e79856e2ecf2cdc6de6b6290768ed3a
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-06-12 18:40:08 +0530

    Create Q07.v

---
+++ VERILOG TEST/TEST-1/Q07.v
@@ -0,0 +1,8 @@
+// Write a Verilog program with the following specifications. 
+// 1. It should have a memory of size 8x14. 
+// 2. Write a task/function to fill all elements in the memory. 
+// 3. Write a method to "clear" to erase the memory 
+// 4. Write a method to add the values stored inside the memory. 
+// 5. Write a method to find the largest number in the memory
+
+
